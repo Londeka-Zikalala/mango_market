66846488a7cf02cbab78c9a942199f078240e8c0
Londeka Zikalala <londekazikalala@gmail.com>
2024-01-18 11:10:15 +0200

    createShop and listShops logic added

--- mango-shopper.js
+++ mango-shopper.js
@@ -1,10 +1,28 @@
  function mangoShopper(db){
 
-	async function createShop(shopName) {
+	 async function createShop(shopName) {
+		 try {
+			  //It must insert a unique shop to the shop table
+		await db.none(`INSERT INTO shop (name) VALUES ($1)
+		ON CONFLICT DO NOTHING`, [shopName]);
+			 
+		 } catch (error) {
+			 console.log(error.message)
+		 }
 		
+
 	}
 
-	async function listShops() {
+	 async function listShops() {
+		 try {
+			//get all the shops from the shop table 
+		let allShops = await db.manyOrNone(`SELECT * FROM shop`);
+		//return the allShopsArray
+		return allShops
+		 }
+		 catch (error) {
+			 console.error(error.message)
+		 }
 		
 	}
 
